c396e6797766549e1f12932aa078ed40c3c6ac86
Enri <Enri@Enri-PC>
2014-09-23 19:11:03 +0200

    upload, download and exit

--- src/main/java/server/FileServer.java
+++ src/main/java/server/FileServer.java
@@ -1,21 +1,22 @@
 package server;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import cli.Shell;
+import util.ChecksumUtils;
 import util.Config;
 import message.Response;
 import message.request.DownloadFileRequest;
 import message.request.InfoRequest;
 import message.request.UploadRequest;
-import message.request.VersionRequest;
+import message.response.DownloadFileResponse;
 import message.response.InfoResponse;
 import message.response.ListResponse;
 import message.response.MessageResponse;
@@ -28,9 +29,10 @@ public class FileServer implements IFileServer {
 	private int proxyUdpPort;
 	private int fileserverAlive;
 	private Shell shell;
+	private TCPListener tcpListener;
+	private UDPPacketSender udpPacketSender;
 	private ExecutorService threadPool;
 	private File[] files;
-
 
 	public FileServer(Config config, Shell shell) {
 		fileserverDir =config.getString("fileserver.dir");
@@ -38,36 +40,63 @@ public class FileServer implements IFileServer {
 		proxyHost = config.getString("proxy.host");
 		proxyUdpPort = config.getInt("proxy.udp.port");
 		fileserverAlive = config.getInt("fileserver.alive");
-		
-		File path = new File(fileserverDir);
-		files = path.listFiles();
-		
 		this.shell = shell;
 		threadPool = Executors.newCachedThreadPool(); //start the thread pool
 	}
 
+	public Shell getShell(){
+		return this.shell;
+	}
+	
+	public TCPListener getTCPListener(){
+		return this.tcpListener;
+	}
+	
+	public UDPPacketSender getUDPPacketSender(){
+		return this.udpPacketSender;
+	}
+	
+	public ExecutorService getThreadPool(){
+		return this.threadPool;
+	}
+
 	public void startFileServer(){
+		readFiles();
 		startShell();
 		startTcpListener();
 		startSendingUdpPackets();
 	}
 
-	private void startSendingUdpPackets() {
-		threadPool.execute(new UDPPacketSender(proxyUdpPort, tcpPort, proxyHost, fileserverAlive));
-		
+	private void readFiles() {
+		File path = new File(fileserverDir);
+		files = path.listFiles();
 	}
 
 	private void startShell(){
-		threadPool.execute(shell); //start the shell thread for stdin
+		//start the shell thread for stdin
+		threadPool.execute(shell); 
 	}
 
 	private void startTcpListener(){
-		threadPool.execute(new TCPListener(tcpPort));
+		//start listening to tcp requests from clients and proxy
+		tcpListener = new TCPListener(tcpPort,this);
+		threadPool.execute(tcpListener);
 	}
+
+	private void startSendingUdpPackets() {
+		//start sending udp alive packets to the proxy
+		udpPacketSender = new UDPPacketSender(proxyUdpPort, tcpPort, proxyHost, fileserverAlive);
+		threadPool.execute(udpPacketSender);
+	}
+
+	/***********************************************************************************************
+	 ******************************** HANDLE REQUESTS FROM THE PROXY *******************************
+	 ***********************************************************************************************/
 
 	@Override
 	public Response list() throws IOException {
-		 Set<String> fileNames = new HashSet<String>();
+		readFiles();
+		Set<String> fileNames = new HashSet<String>();
 		for (File file : files){
 			fileNames.add(file.getName());
 		}
@@ -76,12 +105,25 @@ public class FileServer implements IFileServer {
 
 	@Override
 	public Response download(DownloadFileRequest request) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		readFiles();
+		String user = request.getTicket().getUsername();
+		String filename = request.getTicket().getFilename();
+		File file = new File(fileserverDir + "/" + filename);
+		String checksum = request.getTicket().getChecksum();
+		if (ChecksumUtils.verifyChecksum(user, file, 0, checksum)){
+			byte[] data = new byte[(int) file.length()];
+			FileInputStream fileInputStream = new FileInputStream(file);
+			fileInputStream.read(data);
+			fileInputStream.close();
+
+			return new DownloadFileResponse(request.getTicket(), data);
+		}
+		return new MessageResponse("Failed to verify checksum.");
 	}
 
 	@Override
 	public Response info(InfoRequest request) throws IOException {
+		readFiles();
 		for(int i = 0; i<files.length; i++){
 			if(files[i].getName().equals(request.getFilename())){
 				return new InfoResponse(request.getFilename(),files[i].length());
@@ -91,15 +133,14 @@ public class FileServer implements IFileServer {
 	}
 
 	@Override
-	public Response version(VersionRequest request) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public MessageResponse upload(UploadRequest request) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		File file = new File(fileserverDir + "/" + request.getFilename());
+		file.createNewFile();
+		FileOutputStream fileOutputStream = new FileOutputStream(file);
+		fileOutputStream.write(request.getContent());
+		fileOutputStream.close();
+		readFiles();
+		return new MessageResponse(request.getFilename() + " uploaded to fileserver.");
 	}
 
 }
--- src/main/java/server/FileServerCli.java
+++ src/main/java/server/FileServerCli.java
@@ -3,24 +3,25 @@ package server;
 import java.io.IOException;
 
 import cli.Command;
-import message.Request;
-import message.Response;
-import message.request.InfoRequest;
 import message.response.MessageResponse;
 
 public class FileServerCli implements IFileServerCli {
-	
+
 	private FileServer fileServer;
-	
+
 	public FileServerCli(FileServer fileServer){
 		this.fileServer = fileServer;
 	}
-	
+
 	@Override
 	@Command(value="exit")
 	public MessageResponse exit() throws IOException {
-		//TODO 
-		return null;
+		fileServer.getTCPListener().stopListening();
+		fileServer.getUDPPacketSender().stopSendingPackets();
+		fileServer.getShell().close();
+		System.in.close();
+		fileServer.getThreadPool().shutdown();
+		return new MessageResponse("File server shutdown");
 	}
 
 }
