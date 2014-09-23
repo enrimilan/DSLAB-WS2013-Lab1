da705c75b41b03ed8befb1a05416c061fcab1c85
Enri <Enri@Enri-PC>
2014-09-24 01:55:08 +0200

    tests running ok

--- src/test/java/test/SimpleTest.java
+++ src/test/java/test/SimpleTest.java
@@ -42,18 +42,21 @@ public class SimpleTest {
 			// This should not happen. In case it does, output the stack trace for easier trouble shooting.
 			e.printStackTrace();
 		}
+		
 		try {
 			server.exit();
 		} catch (IOException e) {
 			// This should not happen. In case it does, output the stack trace for easier trouble shooting.
 			e.printStackTrace();
 		}
+		
 		try {
 			client.exit();
 		} catch (IOException e) {
 			// This should not happen. In case it does, output the stack trace for easier trouble shooting.
 			e.printStackTrace();
 		}
+		
 	}
 
 
@@ -62,15 +65,15 @@ public class SimpleTest {
 		String actual = client.login("alice", "12345").toString();
 		String expected = "success";
 		assertTrue(String.format("Response must contain '%s' but was '%s'", expected, actual), actual.contains(expected));
-
+		
 		actual = client.credits().toString();
 		expected = "200";
 		assertTrue(String.format("Response must contain '%s' but was '%s'", expected, actual), actual.contains(expected));
-
+		
 		actual = client.download("short.txt").toString();
 		expected = "!data dslab13";
 		assertTrue(String.format("Response must start with '%s' but was '%s'", expected, actual), actual.startsWith(expected));
-
+		
 		actual = client.credits().toString();
 		expected = "192";
 		assertTrue(String.format("Response must contain '%s' but was '%s'", expected, actual), actual.contains(expected));
@@ -78,13 +81,14 @@ public class SimpleTest {
 		actual = client.upload("upload.txt").toString();
 		expected = "success";
 		assertTrue(String.format("Response must contain '%s' but was '%s'", expected, actual), actual.contains(expected));
-
+		
 		actual = client.credits().toString();
 		expected = "292";
 		assertTrue(String.format("Response must contain '%s' but was '%s'", expected, actual), actual.contains(expected));
-
+		
 		actual = client.logout().toString();
 		expected = "Successfully logged out.";
 		assertTrue(String.format("Response must contain '%s' but was '%s'", expected, actual), actual.contains(expected));
+		
 	}
 }
